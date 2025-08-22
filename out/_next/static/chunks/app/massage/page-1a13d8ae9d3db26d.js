1ac8fc446374a154006abba07eca431151a15d0f
mounkaila <Mounkaila144@gmail.com>
2025-08-22 18:09:01 +0100

    init

---
+++ out/_next/static/chunks/app/massage/page-1a13d8ae9d3db26d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[846,114],{3152:function(e,n,s){Promise.resolve().then(s.bind(s,538)),Promise.resolve().then(s.t.bind(s,4724,23))}},function(e){e.O(0,[507,724,538,971,864,744],function(){return e(e.s=3152)}),_N_E=e.O()}]);
